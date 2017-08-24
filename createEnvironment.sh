2457cef911fa3186d34f92bf4ee31f0a9d4aa74a
a427538 <karl-johannes.stich@atos.net>
2017-08-24 15:05:27 +0200

    Update createEnvironment.sh

--- createEnvironment.sh
+++ createEnvironment.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 domain=mobilelife.de
 fp_sshkey=f2:6f:8b:32:a7:60:96:a1:43:d8:65:d9:d7:07:aa:3a
-ImageID=`doctl compute image list | grep centos7ocp | awk '{print $1}'`
-for host in master1 master2 master3 etcd1 etcd2 etcd3 node1 node2 node3
+ImageID=`doctl compute image list | grep centos7ocp-nodocker | awk '{print $1}'`
+for host in lb etcd1 etcd2 etcd3
+	./createDroplet.sh $host $ImageID &
+done
+ImageID=`doctl compute image list | grep centos7ocp-docker | awk '{print $1}'`
+for host in master1 master2 master3 node1 node2 node3
 do
 	./createDroplet.sh $host $ImageID &
 done
